0903428ffbff0f5e1b7cf3be712ce2050aa09fbe
xavsln <test@test.com>
2023-05-24 17:14:30 +0200

    ExerciseWeek3 deployed to Goerli

--- contracts/ExerciseWeek3.sol
+++ contracts/ExerciseWeek3.sol
@@ -1,3 +1,21 @@
+/*
+1. Contract was deployed to Goerli testnet on 24th of May 2023:
+
+. Using Remix IDE (Injected Web3 / Metamask)
+
+2. Contract address is:
+
+3. Contract is accessible here: https://goerli.etherscan.io/address/0xec922fafff3c1171decac05efa1eae75baf0dcf1
+
+4. Contract was verified and published on Etherscan and it is possible to interact with it:
+
+. Queries (to read) are available from here: https://goerli.etherscan.io/address/0xec922fafff3c1171decac05efa1eae75baf0dcf1#code
+. Change to state variables can be done via Remix IDE
+
+*/
+
+
+
 // SPDX-License-Identifier: Unlicence
 
 pragma solidity >=0.6.0 <0.8.0;
